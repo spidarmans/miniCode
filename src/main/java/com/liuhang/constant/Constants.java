599b611c9078c1b9066a3d1a3174215e36fcd879
spidarmans <694241132@qq.com>
2019-02-27 22:20:27 +0800

    first commt

---
+++ src/main/java/com/liuhang/constant/Constants.java
@@ -0,0 +1,15 @@
+package com.liuhang.constant;
+
+public class Constants {
+	/**被除数3**/
+	public static final int THREE = 3;
+	/**被除数5**/
+	public static final int FIVE = 5;
+	/**整除结果0**/
+	public static final int ZERO = 0;
+	/**Buzz**/
+	public static final String BUZZ = "Buzz";
+	/**Fizz**/
+	public static final String FIZZ = "Fizz";
+
+}
---
+++ src/main/java/com/liuhang/test/MiniCodeTest.java
@@ -0,0 +1,101 @@
+package com.liuhang.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.alibaba.fastjson.JSON;
+import com.liuhang.constant.Constants;
+
+public class MiniCodeTest {
+	/**
+	 * 存放Fizz数字的list集合
+	 */
+	private static List<Integer> fizzList = new ArrayList<Integer>();
+	/**
+	 * 存放Buzz数字的list集合
+	 */
+	private static List<Integer> buzzList = new ArrayList<Integer>();
+
+	/**
+	 * 存放FizzBuzz数字的list集合
+	 */
+	private static List<Integer> fizzBuzzList = new ArrayList<Integer>();
+	
+	/**
+	 * 临时存放游戏规则的结果数字
+	 */
+	private static String tempNum = "";
+
+	
+	public static void main(String[] args) {
+		/*遍历1-100自然数*/
+		for (int num = 1; num <= 100; num++) {
+			/*打印1-100游戏结果*/
+			printGameNum(num);
+			if(fizzBuzzCondition(num)){
+				fizzBuzzList.add(num);
+				continue ;
+			}
+			if(fizzCondition(num)){
+				fizzList.add(num);
+			}
+			if(buzzCondition(num)){
+				buzzList.add(num);
+				continue ;
+			}
+		}
+		fizzList.addAll(fizzBuzzList);
+		buzzList.addAll(fizzBuzzList);
+		//list排序
+		Collections.sort(fizzList);
+		Collections.sort(buzzList);
+		System.out.println();
+		System.out.println("Fizz或含有3的数字个数："+fizzList.size() +"  数值打印："+JSON.toJSONString(fizzList));
+		System.out.println("Bizz或含有5的数字个数："+buzzList.size() +"  数值打印："+JSON.toJSONString(buzzList));
+		System.out.println("FizzBuzz个数："+fizzBuzzList.size() +"  数值打印："+JSON.toJSONString(fizzBuzzList));
+		
+	}
+	/**
+	 * 打印1-100游戏结果
+	 * @param num
+	 */
+	private static void printGameNum(int num) {
+		tempNum = String.valueOf(num);
+		if(num%Constants.FIVE==Constants.ZERO){
+			tempNum = Constants.BUZZ;
+		}
+		if(num%Constants.THREE==Constants.ZERO){
+			tempNum = Constants.FIZZ;
+		}
+		System.out.print(tempNum+" ");
+	}
+ 
+	/**
+	 * 被5整除或含有5的自然数判断条件
+	 * @param num
+	 * @return
+	 */
+	private static boolean buzzCondition(int num) {
+		return num%Constants.FIVE==Constants.ZERO || String.valueOf(num).indexOf(String.valueOf(Constants.FIVE))>=0;
+	}
+
+	/**
+	 * 被3整除或含有3的自然数判断条件
+	 * @param num
+	 * @return
+	 */
+	private static boolean fizzCondition(int num) {
+		return num%Constants.THREE==Constants.ZERO || String.valueOf(num).indexOf(String.valueOf(Constants.THREE))>=0;
+	}
+	
+	/**
+	 * FizzBuzz判断条件
+	 * @param num
+	 * @return
+	 */
+	private static boolean fizzBuzzCondition(int num) {
+		return num%Constants.THREE==Constants.ZERO && num%Constants.FIVE==Constants.ZERO;
+	}
+
+}
