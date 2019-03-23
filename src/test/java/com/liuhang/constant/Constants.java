bb0f0f1270b2ea25397676ce18c37cd2763fbabd
spidarmans <694241132@qq.com>
2019-03-23 11:12:01 +0800

    qqqqq

--- src/test/java/com/liuhang/constant/Constants.java
+++ src/test/java/com/liuhang/constant/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	/**被除数5**/
 	public static final int FIVE = 5;
 	/**整除结果0**/
-	public static final int ZERO = 0;
+	public static final int ZERO = 10;
 	/**Buzz**/
 	public static final String BUZZ = "Buzz";
 	/**Fizz**/
